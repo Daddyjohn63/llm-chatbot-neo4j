5b13831e1b0127457b1696f68a377427f1f291d8
Daddyjohn63 <john@webandprosper.co.uk>
2025-07-05 11:26:45 +0100

    answer chain generation. Some imports were wrong and in answer-generation.chain.ts had to wrap await in async.

--- src/modules/agent/chains/answer-generation.chain.ts
+++ src/modules/agent/chains/answer-generation.chain.ts
@@ -1,7 +1,10 @@
-import { StringOutputParser } from "@langchain/core/output_parsers";
-import { PromptTemplate } from "@langchain/core/prompts";
-import { RunnableSequence } from "@langchain/core/runnables";
-import { BaseLanguageModel } from "langchain/base_language";
+import { BaseLanguageModel } from '@langchain/core/language_models/base';
+import { StringOutputParser } from '@langchain/core/output_parsers';
+import { PromptTemplate } from '@langchain/core/prompts';
+import { RunnableSequence } from '@langchain/core/runnables';
+import { OpenAI } from '@langchain/openai';
+//import OpenAI from 'openai';
+//import { BaseLanguageModel } from "langchain/base_language";
 
 // tag::interface[]
 export interface GenerateAnswerInput {
@@ -14,12 +17,43 @@ export interface GenerateAnswerInput {
 export default function initGenerateAnswerChain(
   llm: BaseLanguageModel
 ): RunnableSequence<GenerateAnswerInput, string> {
+  const answerQuestionPrompt = PromptTemplate.fromTemplate(`
+    Use only the following context to answer the following question.
+
+    Question:
+    {question}
+
+    Context:
+    {context}
+
+    Answer as if you have been asked the original question.
+    Do not use your pre-trained knowledge.
+
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    Include links and sources where possible.
+    `);
   // TODO: Create a Prompt Template
   // const answerQuestionPrompt = PromptTemplate.fromTemplate(`
   // TODO: Return a RunnableSequence
-  // return RunnableSequence.from<GenerateAnswerInput, string>([])
+  return RunnableSequence.from<GenerateAnswerInput, string>([
+    answerQuestionPrompt,
+    llm,
+    new StringOutputParser()
+  ]);
 }
 // end::function[]
+
+const llm = new OpenAI(); // Or the LLM of your choice
+const answerChain = initGenerateAnswerChain(llm);
+
+async function run() {
+  const output = await answerChain.invoke({
+    question: 'Who is the CEO of Neo4j?',
+    context: 'Neo4j CEO: Emil Eifrem'
+  });
+  console.log(output);
+}
+run();
 
 /**
  * How to use this chain in your application:
--- src/modules/agent/chains/speculative-answer-generation.chain.test.ts
+++ src/modules/agent/chains/speculative-answer-generation.chain.test.ts
@@ -1,26 +1,27 @@
-import { config } from "dotenv";
-import initGenerateAnswerChain from "./answer-generation.chain";
-import { BaseChatModel } from "langchain/chat_models/base";
-import { RunnableSequence } from "@langchain/core/runnables";
-import { ChatOpenAI } from "@langchain/openai";
-import { PromptTemplate } from "@langchain/core/prompts";
-import { StringOutputParser } from "@langchain/core/output_parsers";
+import { config } from 'dotenv';
+import initGenerateAnswerChain from './answer-generation.chain';
+//import { BaseChatModel } from "langchain/chat_models/base";
+import { RunnableSequence } from '@langchain/core/runnables';
+import { ChatOpenAI } from '@langchain/openai';
+import { PromptTemplate } from '@langchain/core/prompts';
+import { StringOutputParser } from '@langchain/core/output_parsers';
+import { BaseChatModel } from '@langchain/core/language_models/chat_models';
 
-describe("Speculative Answer Generation Chain", () => {
+describe('Speculative Answer Generation Chain', () => {
   let llm: BaseChatModel;
   let chain: RunnableSequence;
   let evalChain: RunnableSequence<any, any>;
 
   beforeAll(async () => {
-    config({ path: ".env.local" });
+    config({ path: '.env.local' });
 
     llm = new ChatOpenAI({
       openAIApiKey: process.env.OPENAI_API_KEY,
-      modelName: "gpt-3.5-turbo",
+      modelName: 'gpt-3.5-turbo',
       temperature: 0,
       configuration: {
-        baseURL: process.env.OPENAI_API_BASE,
-      },
+        baseURL: process.env.OPENAI_API_BASE
+      }
     });
 
     chain = await initGenerateAnswerChain(llm);
@@ -36,51 +37,51 @@ describe("Speculative Answer Generation Chain", () => {
         Respond simply with "yes" or "no".
       `),
       llm,
-      new StringOutputParser(),
+      new StringOutputParser()
     ]);
     // end::evalchain[]
   });
 
-  describe("Simple RAG", () => {
-    it("should use context to answer the question", async () => {
-      const question = "Who directed the matrix?";
+  describe('Simple RAG', () => {
+    it('should use context to answer the question', async () => {
+      const question = 'Who directed the matrix?';
       const response = await chain.invoke({
         question,
-        context: '[{"name": "Lana Wachowski"}, {"name": "Lilly Wachowski"}]',
+        context: '[{"name": "Lana Wachowski"}, {"name": "Lilly Wachowski"}]'
       });
 
       // tag::eval[]
       const evaluation = await evalChain.invoke({ question, response });
 
-      expect(`${evaluation.toLowerCase()} - ${response}`).toContain("yes");
+      expect(`${evaluation.toLowerCase()} - ${response}`).toContain('yes');
       // end::eval[]
     });
 
-    it("should refuse to answer if information is not in context", async () => {
-      const question = "Who directed the matrix?";
+    it('should refuse to answer if information is not in context', async () => {
+      const question = 'Who directed the matrix?';
       const response = await chain.invoke({
         question,
         context:
-          "The Matrix is a 1999 science fiction action film starring Keanu Reeves",
+          'The Matrix is a 1999 science fiction action film starring Keanu Reeves'
       });
 
       const evaluation = await evalChain.invoke({ question, response });
-      expect(`${evaluation.toLowerCase()} - ${response}`).toContain("no");
+      expect(`${evaluation.toLowerCase()} - ${response}`).toContain('no');
     });
 
-    it("should answer this one??", async () => {
-      const role = "The Chief";
+    it('should answer this one??', async () => {
+      const role = 'The Chief';
 
-      const question = "What was Emil Eifrems role in Neo4j The Movie??";
+      const question = 'What was Emil Eifrems role in Neo4j The Movie??';
       const response = await chain.invoke({
         question,
-        context: `{"Role":"${role}"}`,
+        context: `{"Role":"${role}"}`
       });
 
       expect(response).toContain(role);
 
       const evaluation = await evalChain.invoke({ question, response });
-      expect(`${evaluation.toLowerCase()} - ${response}`).toContain("yes");
+      expect(`${evaluation.toLowerCase()} - ${response}`).toContain('yes');
     });
   });
 });
